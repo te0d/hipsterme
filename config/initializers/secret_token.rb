d2566e13ab324b70459e28a6868cde32d152478e
Tom ODonnell <todizzle91@gmail.com>
2012-12-26 14:45:16 -0500

    first post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipsterme::Application.config.secret_token = 'f1c6b5b9746e3fc26b7e001ce791f9a045f5619ea91f75223518d06668d2b4f25beebae82cf78f4448da0be22d4a2e54c8399170b3080ed88a1af40c840c977c'
